391f1769f510c1ee4ab755647af7411cbdd0ac7d
pasindumalinda994a-del <pasindumalinda994a@gmail.com>
2026-05-14 21:35:23 +0530

    update responsivness

--- app/sections/home/Hero.tsx
+++ app/sections/home/Hero.tsx
@@ -118,8 +118,8 @@ export default function Hero({ content }: HeroProps) {
               >
                 <H1
                   textSize={{
-                    mobile: "5.25rem",
-                    tablet: "4.5rem",
+                    mobile: "5.05rem",
+                    tablet: "10.25rem",
                     desktop: "17rem",
                   }}
                   className="max-w-8xl bg-[linear-gradient(to_bottom,_#ffffff,_rgba(255,255,255,0.95),_rgba(255,255,255,0.55),_rgba(255,255,255,0.08),_transparent)] bg-clip-text text-transparent filter blur-[0.5px]"
